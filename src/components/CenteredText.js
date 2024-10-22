0f55fc3579b0f8f923b528eb17cd1938b563e3b0
Voila Vitamin <148288760+voilaVitamin@users.noreply.github.com>
2024-10-22 15:28:58 -0400

    linked msrcad

--- src/components/CenteredText.js
+++ src/components/CenteredText.js
@@ -5,9 +5,9 @@ const CenteredText = () => {
   return (
     <div className="centered-text">
       <p>Gulegulzar Technologies</p>
-      
       <button><a href="https://github.com/gulegulzartechnologies/repscrap">Repository Scrapper</a></button>
-      
+      <br></br>
+      <button><a href="https://msrcad.github.io">MSRCAD</a></button>
     </div>
   );
 };
